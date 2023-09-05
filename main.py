825b14e0ced3107e31557dfa1bcfe7f7c5efc60f
firexrwt <stepanveremeev@gmail.com>
2023-09-05 16:23:46 +0200

    corrected main.py and removed a cycle

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} is ready!")
-    while True:
-        continue
 
 
 @bot.event
